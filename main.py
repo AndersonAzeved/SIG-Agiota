8990bac6df48eade80288af23cb6090083921475
AndersonAzeved <andersonsilva14.2017@gmail.com>
2022-12-19 16:13:42 -0300

    up

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import config
 sistema = {}
 
 op = input("Informe a opção: ")
-if op == "1":
+while op != "0":
     sistema = config.LerArquivo("files/sistema.dat",sistema)
-    config.CadastrarUsuario(sistema)
-    config.SalvaArquivo("files/sistema.dat",sistema)
+    if op == "1":
+        config.CadastrarUsuario(sistema)
+        config.SalvaArquivo("files/sistema.dat",sistema)
+    elif op == "2":
+        print("Parte 2")
