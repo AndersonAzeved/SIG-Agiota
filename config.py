e6e651ceb6b660a7a31072977a31c7b9ec9cea98
AndersonAzeved <andersonsilva14.2017@gmail.com>
2022-12-19 12:35:17 -0300

    função criada WhileCpf

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
+import biblioteca
+
 def CadastrarUsuario():
     sistema = {}
     print("Cadastramento do Usuário")
-    cpf = input("CPF: ").replace(".",",","-","/")
+    cpf = input("CPF: ")
+    cpf = WhileCpf(cpf)
     nome = input("Nome: ")
     sistema[cpf] = [nome]
-    
+
+def WhileCpf(cpf):
+    if not biblioteca.ValidaCpf(cpf):
+        while not biblioteca.ValidaCpf(cpf):
+            cpf = input("CPF Inválido! Tente Novamente: ")
+    return cpf
