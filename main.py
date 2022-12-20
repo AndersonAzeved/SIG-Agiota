7045a8c21c94bb22976660fc59418d163d363d04
AndersonAzeved <andersonsilva14.2017@gmail.com>
2022-12-20 11:43:09 -0300

    atualização

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ sistema = {}
 cartao = {}
 
 op = input(""
-            "////////////////////////////////////\n"
-            "///                              ///\n"
-            "///   1. Cadastrar Devedor       ///\n"
-            "///   2. Cadastrar Dívidas       ///\n"
-            "///   3. Cadastrar Cartão        ///\n"
-            "///                              ///\n"
-            "////////////////////////////////////\n"
-            "Informe a opção: ")
+    "////////////////////////////////////\n"
+    "///                              ///\n"
+    "///         SIG-AGIOTA           ///\n"
+    "///                              ///\n"
+    "///                  by Anderson ///\n"
+    "////////////////////////////////////\n"
+    "///                              ///\n"
+    "///   1. Cadastrar Devedor       ///\n"
+    "///   2. Cadastrar Dívidas       ///\n"
+    "///   3. Cadastrar Cartão        ///\n"
+    "///   0. Sair                    ///\n"
+    "///                              ///\n"
+    "////////////////////////////////////\n"
+    "Informe a opção: ")
 while op != "0":
     sistema = config.LerArquivo("files/sistema.dat",sistema)
     if op == "1":
@@ -43,3 +49,13 @@ while op != "0":
             "")
         config.CadastrarCartao(cartao)
         config.SalvaArquivo("files/cartao.dat",cartao)
+    
+    op = input(""
+            "////////////////////////////////////\n"
+            "///                              ///\n"
+            "///   1. Cadastrar Devedor       ///\n"
+            "///   2. Cadastrar Dívidas       ///\n"
+            "///   3. Cadastrar Cartão        ///\n"
+            "///                              ///\n"
+            "////////////////////////////////////\n"
+            "Informe a opção: ")
