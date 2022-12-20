8cddff303d11cf93a310d68d78aaf424c4e6976c
AndersonAzeved <andersonsilva14.2017@gmail.com>
2022-12-20 09:58:33 -0300

    iniciando CadastrarDividas

--- config.py
+++ config.py
@@ -1,21 +1,24 @@
 import biblioteca
 import pickle
 
-def CadastrarUsuario(dicionario):
-    print("Cadastramento do Usuário")
-    cpf = input("CPF: ")
-    cpf = TirarPontosCpf(WhileCpf(cpf,dicionario))
-    nome = input("Nome: ")
-    dicionario[cpf] = [nome,{}]
+def CadastrarDevedor(dicionario):
+  print("Cadastramento do Devedor")
+  cpf = input("CPF: ")
+  cpf = TirarPontosCpf(WhileCpf(cpf,dicionario,1))
+  nome = input("Nome: ")
+  dicionario[cpf] = [nome,{}]
 
-def WhileCpf(cpf,dicionario):
-    if not biblioteca.ValidaCpf(cpf):
-        while not biblioteca.ValidaCpf(cpf):
-            cpf = TirarPontosCpf(input("CPF Inválido! Tente Novamente: "))
-    if VerificarIndice(dicionario,cpf):
-        while VerificarIndice(dicionario,cpf):
-            cpf = TirarPontosCpf(input("CPF Já Cadastrado! Tente Novamente: "))
-    return cpf
+def WhileCpf(cpf,dicionario,op):
+  if not biblioteca.ValidaCpf(cpf):
+      while not biblioteca.ValidaCpf(cpf):
+          cpf = TirarPontosCpf(input("CPF Inválido! Tente Novamente: "))
+  if VerificarIndice(dicionario,cpf):
+      while VerificarIndice(dicionario,cpf):
+        if op == 1:
+          cpf = TirarPontosCpf(input("CPF Já Cadastrado! Tente Novamente: "))
+        else:
+          cpf = TirarPontosCpf(input("CPF Não Cadastrado! Tente Novamente: "))
+  return cpf
 
 def SalvaArquivo(nome_arquivo, dicionario):
   arquivo = open(nome_arquivo, "ab")
@@ -35,13 +38,28 @@ def LerArquivo(nome_arquivo,dicionario):
   return dicionario
     
 def VerificarIndice(dicionario,indice):
-    if indice in dicionario:
-        return True
-    else:
-        return False
+  if indice in dicionario:
+      return True
+  else:
+      return False
 
 def TirarPontosCpf(cpf):
-    cpf = cpf.replace('.', '')
-    cpf = cpf.replace('-', '')
-    cpf = cpf.replace(' ', '')
-    return cpf
+  cpf = cpf.replace('.', '')
+  cpf = cpf.replace('-', '')
+  cpf = cpf.replace(' ', '')
+  return cpf
+
+def CadastrarDividas(dicionario):
+  cpf = TirarPontosCpf(input("Informe o CPF do Devedor: "))
+  cpf = WhileCpf(cpf, dicionario, 2)
+  nome_compra = input("Nome da Compra: ")
+  valor = input("Valor da Compra: ")
+  op = input("Pagamento a prazo (SIM-S ou NÃO-N): ").lower()
+  while not(op == "sim" or op == "s") or not(op == "não" or op == "n" or op == "nao"):
+    op = input("Informação Inválida, Tente Novamente!\nPagamento a prazo (SIM-S ou NÃO-N): ").lower()
+  if op == "sim" or op == "s":
+    prazo = input("Em quantas vezes? ")
+    parcela = input("Valor das Parcelas: ")
+  else:
+    parcela = input("Valor àvista: ")
+  data = input("Informe a data da compra: ") 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ op = input("Informe a opção: ")
 while op != "0":
     sistema = config.LerArquivo("files/sistema.dat",sistema)
     if op == "1":
-        config.CadastrarUsuario(sistema)
+        config.CadastrarDevedor(sistema)
         config.SalvaArquivo("files/sistema.dat",sistema)
     elif op == "2":
-        print("Parte 2")
+        config.CadastrarDividas()
